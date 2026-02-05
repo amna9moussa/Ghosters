b56596f89c264348e3e6ac1cd0b6a829e6e6c0f4
Amna <amna3mousa@gmail.com>
2026-02-05 15:19:16 +0300

    add the models

---
+++ Ghosters/Scenes/Result.swift
@@ -0,0 +1,8 @@
+//
+//  Result.swift
+//  Ghosters
+//
+//  Created by Amna  on 17/08/1447 AH.
+//
+
+import Foundation
---
+++ MainMenuScene.swift
@@ -0,0 +1,8 @@
+//
+//  MainMenuScene.swift
+//  Ghosters
+//
+//  Created by Amna  on 17/08/1447 AH.
+//
+
+import Foundation
